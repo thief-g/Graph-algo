30038fc8a544614689b71e8487ffd4800c3efbf9
thief-g <92672068+thief-g@users.noreply.github.com>
2021-10-20 12:04:58 +0530

    Update graph-algo.cpp

--- graph-algo.cpp
+++ graph-algo.cpp
@@ -1,1 +1,14 @@
 // pseudo code of ll algorithms here...
+// DFS...
+vector<vector<int>> adj; // graph represented as an adjacency list
+int n; // number of vertices
+
+vector<bool> visited;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adj[v]) {
+        if (!visited[u])
+            dfs(u);
+    }
+}
